d197fea934ef9539d092e57d7ff674ff2a310b86
akashb1998 <akashb1998@gmail.com>
2018-10-17 22:54:08 +0530

    DLA with Anisotropy introduced to the pattern
    Simulation of Diffusion Limited Aggregation with anisotropic attachment of particles to the center.

---
+++ DLAanisotropy.m
@@ -0,0 +1,52 @@
+n = 101;
+M = zeros(n, n);
+M(51, 51) = 1;
+k = 0;
+
+while(k<500)
+    theta = 2*pi*rand();
+    i = floor(51 + 40*cos(theta));
+    j = floor(51 + 40*sin(theta));
+while(1)
+if(M(i-1,j) == 1)
+    M(i,j) = 1;
+    k = k + 1; 
+    break
+end
+
+if(M(i,j+1) == 1)
+    M(i,j) = 1;
+    k = k + 1; 
+    break
+end
+
+if(M(i,j-1) == 1)
+    M(i,j) = 1;
+    k = k + 1; 
+    break
+end
+
+if(M(i+1,j) == 1)
+    M(i,j) = 1;
+    k = k + 1; 
+    break
+end
+
+random = ceil(8*rand());
+neighbours = [i-1 j+1;i-1 j;i-1 j-1;i j+1;i j-1;i+1 j+1;i+1 j;i+1 j-1];
+inew = neighbours(random,1);
+jnew = neighbours(random,2);
+
+x_dist = inew - 51;
+y_dist = jnew - 51;
+
+if((x_dist^2 + y_dist^2)>1600)
+    break
+end
+
+i = inew;
+j = jnew;
+end
+end
+imshow(M)
+title("Anisotropic Attachment of Particles to Center")
